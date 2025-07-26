7fcf8b48d5677c08ede1c8b6af757d79a6daa27b
Tony Nouhra <tony.nouhra@montymobile.com>
2025-07-27 02:17:06 +0300

    Add MyST Parser support and update Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
 import sys
@@ -11,18 +8,14 @@ from pathlib import Path
 sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "vet-core"
 copyright = "2025, Vet Clinic Platform Team"
 author = "Vet Clinic Platform Team"
 
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
+# The version info for the project you're documenting
 try:
     from vet_core import __version__
-
     version = __version__
     release = __version__
 except ImportError:
@@ -30,7 +23,6 @@ except ImportError:
     release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -40,13 +32,32 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx_autodoc_typehints",
+    "myst_parser",  # Added: Support for Markdown files
 ]
+
+# MyST Parser configuration
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
+
+# File patterns
+source_suffix = {
+    '.rst': None,
+    '.md': None,
+}
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
